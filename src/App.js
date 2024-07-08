7f6d4bd338d3082b7ec87367897d17cb1894e2cd
Gundeep Singh Saluja <gssaluja77@gmail.com>
2024-07-08 14:21:49 -0700

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import Visualizer from './components/visualizer';
-import './App.css';
+import React from "react";
+import Visualizer from "./components/visualizer";
+import "./App.css";
+import Footer from "./footer";
 
 class App extends React.Component {
-	
-    render() { 
-        return (
-            <Visualizer />
-        );
-	}
+  render() {
+    return (
+      <>
+        <Visualizer />
+        <Footer />
+      </>
+    );
+  }
 }
- 
+
 export default App;
---
+++ src/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+// Footer Component
+const Footer = () => {
+  return (
+    <footer>
+      <p>Sorting Algorithm Visualizer</p>
+    </footer>
+  );
+};
+
+export default Footer;
