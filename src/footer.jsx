8b68d85fd8b77fe79f06c352e0c8dd1e2d05e469
Gundeep Singh Saluja <gssaluja77@gmail.com>
2024-07-18 14:52:19 -0700

    Added a typewriter effect to footer text

--- src/footer.jsx
+++ src/footer.jsx
@@ -1,10 +1,20 @@
 import React from "react";
-
+import Typewriter from "typewriter-effect";
 // Footer Component
 const Footer = () => {
   return (
     <footer>
-      <p>Sorting Algorithm Visualizer</p>
+      <p>Sorting Wizard</p>
+      <Typewriter
+        options={{
+          strings:
+            "Visualize various sorting algorithms in real-time to clearly understand how they work!",
+          autoStart: true,
+          loop: false,
+          cursor: "",
+          delay: 75,
+        }}
+      />
     </footer>
   );
 };
