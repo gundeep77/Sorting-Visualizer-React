8c645814331578827711d8547fa36de9b07bbc2f
Gundeep Singh Saluja <gssaluja77@gmail.com>
2024-07-18 15:10:59 -0700

    Displayed numbers on the bars.

--- src/components/frame.jsx
+++ src/components/frame.jsx
@@ -1,28 +1,31 @@
-import React from 'react';
+import React from "react";
 
 class Frame extends React.Component {
-    render() { 
-        return (
-            <div className="frame">
-                <div className="array">
-                    {this.props.list.map((element, index) => (
-                        <div
-                            className = {this.getClass(element.classType)}
-                            key = {index}
-                            style = {{height : `${4*element.key}px`}}
-                            value = {element.key}>
-                        </div>
-                    ))}
-                </div>
+  render() {
+    return (
+      <div className="frame">
+        <div className="array">
+          {this.props.list.map((element, index) => (
+            <div
+              className={this.getClass(element.classType)}
+              key={index}
+              style={{ height: `${4 * element.key}px`, display: "flex", alignItems: "center", justifyContent: "center"}}
+              value={element.key}
+            >
+              
+              <p>{element.key}</p>
             </div>
-        );
-    }
+          ))}
+        </div>
+      </div>
+    );
+  }
 
-    getClass = (value) => {
-        if(value === 0) return 'cell';
-        else if(value === 1) return 'cell current';
-        return 'cell done';
-    };
+  getClass = (value) => {
+    if (value === 0) return "cell";
+    else if (value === 1) return "cell current";
+    return "cell done";
+  };
 }
- 
+
 export default Frame;
